b977cefc2b34c9693cbe7030bfc9ff6ede4321dd
Sanchez Kim <gavrylovalexandr@gmail.com>
2025-03-27 23:32:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0
+console.log(document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0)
