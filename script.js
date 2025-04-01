3fc22db90a6e460fe7cedb0b00d83bce61a818a0
Sanchez Kim <gavrylovalexandr@gmail.com>
2025-04-01 08:19:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-// document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0
-console.log(document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0)
